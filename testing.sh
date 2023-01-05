91a3cccb6c426de1f45095c48fb62012643b034b
Javier12301 <javierramirez1230123@gmail.com>
2023-01-05 18:13:36 -0300

    Update

--- testing.sh
+++ testing.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-hash="1f22aa08acc027588ba1b848d48f6e2bbaff79b99600a8d5b9fd49f35e20b0c0"
+nombre=""
 
-eliminar_fecha=$(cat ut.tmp | grep "$hash" -B 1 | grep "hash *" | grep -Eo ":[0-9]*.*" | grep -o '[0-9].*' | grep -m3 -o '[0-9].*' | grep -Eo '[0-9][0-9]*:')
+function prueba(){
+	hash="$1" 
 
-bash_original=$(cat ut.tmp | grep "$hash" -B 1 | grep "hash *" | grep -Eo ":[0-9]*.*" | grep -o '[0-9].*' | grep -m3 -o '[0-9].*' | grep -v $eliminar_fecha)
+	eliminar_fecha=$(cat ut.tmp | grep "$hash" -B 1 | grep "hash *" | grep -Eo ":[0-9]*.*" | grep -o '[0-9].*' | grep -m3 -o '[0-9].*' | grep -Eo '[0-9][0-9]*:')
 
-bitcoin=$(cat ut.tmp | grep "$hash" -B 1 | grep "hash *" | grep -Eo ":[0-9]*.*" | grep -o '[0-9].*' | grep -m3 -o '[0-9].*' | grep -Eo "[$eliminar_fecha]*[0-9]*.[0-9]*" | grep -Eo ":[0-9]*.*" | grep -Eo "[0-9].*" | cut -c 3-13)
+	bash_original=$(cat ut.tmp | grep "$hash" -B 1 | grep "hash *" | grep -Eo ":[0-9]*.*" | grep -o '[0-9].*' | grep -m3 -o '[0-9].*' | grep -v $eliminar_fecha)
 
-echo $bitcoin"BTC"
+	bitcoin=$(cat ut.tmp | grep "$hash" -B 1 | grep "hash *" | grep -Eo ":[0-9]*.*" | grep -o '[0-9].*' | grep -m3 -o '[0-9].*' | grep -Eo "[$eliminar_fecha]*[0-9]*.[0-9]*" | grep -Eo ":[0-9]*.*" | grep -Eo "[0-9].*" | cut -c 3-13)
 
+
+}
+
+prueba "ef4f548a905f64ceda05c1e603c449ea1b4d0741992305ebbe7fccf9fcf03c9c" && echo $bitcoin "BTC"
 
 
